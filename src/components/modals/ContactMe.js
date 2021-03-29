f9649b24c96e922cf21f143bdb8ee82b24d62264
Guy Berkovich <mailofguy@gmail.com>
2021-03-29 14:34:09 +0300

    fix console errors

--- src/components/modals/ContactMe.js
+++ src/components/modals/ContactMe.js
@@ -23,11 +23,11 @@ const ContactMe = ({ isOpen, toggle }) => {
     }
 
     const onChange = (evt) => {
-        updateFormValue({ ...contactForm, [evt.target.id]: evt.target.value});
+        updateFormValue({ ...contactForm, [evt.target.id]: evt.target.value });
     }
 
     return (
-        <Modal isOpen={isOpen} toggle={toggle} backdrop centered keyboard size="lg">
+        <Modal isOpen={ isOpen } toggle={ toggle } backdrop centered keyboard size="lg">
             <ModalHeader className='contact-me-header'>Get In Touch</ModalHeader>
             <ModalBody className='contact-me-body'>
                 <section className='left-block'>
@@ -49,23 +49,23 @@ const ContactMe = ({ isOpen, toggle }) => {
                 <section className='right-block'>
                     <Form>
                         <FormGroup>
-                            <input type='email' id='email' placeholder='jhon@example.com' onChange={onChange} />
-                            <label for='email'>Email</label>
+                            <input type='email' id='email' placeholder='jhon@example.com' onChange={ onChange } />
+                            <label htmlFor='email'>Email</label>
                         </FormGroup>
                         <FormGroup>
-                            <input type='text' id='name' placeholder='Jhon doe' onChange={onChange} />
-                            <label for='name'>Name</label>
+                            <input type='text' id='name' placeholder='Jhon doe' onChange={ onChange } />
+                            <label htmlFor='name'>Name</label>
                         </FormGroup>
                         <FormGroup>
-                            <input type='textarea' id='message' placeholder='type your message here..' onChange={onChange} />
-                            <label for='message'>Message</label>
+                            <input type='textarea' id='message' placeholder='type your message here..' onChange={ onChange } />
+                            <label htmlFor='message'>Message</label>
                         </FormGroup>
                     </Form>
                 </section>
             </ModalBody>
             <ModalFooter className='contact-me-footer'>
-                <Button color='secondary' onClick={toggle}>Cancel</Button>
-                <Button color='primary' onClick={submit}>Send</Button>
+                <Button color='secondary' onClick={ toggle }>Cancel</Button>
+                <Button color='primary' onClick={ submit }>Send</Button>
             </ModalFooter>
         </Modal>
     )
