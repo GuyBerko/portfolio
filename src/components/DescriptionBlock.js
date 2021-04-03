8291450c2cd7885b84e0d7c8c48a90bb045aac1a
Guy Berkovich <mailofguy@gmail.com>
2021-04-03 13:57:04 +0300

    add non selectable class

--- src/components/DescriptionBlock.js
+++ src/components/DescriptionBlock.js
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 
 export const DescriptionBlock = ({ onToggleModal }) => {
     return (
-        <div className='main-description'>
+        <div className="main-description nonselectable">
             <h1>Hello!</h1>
             <h2>I am Guy Berkovich</h2>
             <h3>A FullStack Developer</h3>
             <div className="buttons-container">
-                <Button size="lg" outline color="secondary" onClick={onToggleModal.bind(this, 'aboutModal')}>About Me</Button>
-                <Button size="lg" outline color="secondary" onClick={onToggleModal.bind(this, 'contactModal')}>Contact Me</Button>
+                <Button size="lg" outline color="secondary" onClick={ onToggleModal.bind(this, 'aboutModal') }>About Me</Button>
+                <Button size="lg" outline color="secondary" onClick={ onToggleModal.bind(this, 'contactModal') }>Contact Me</Button>
             </div>
         </div>
     )
