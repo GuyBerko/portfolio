e953e841230e5d38af7f5e1f744cf973eba4175f
Guy Berkovich <mailofguy@gmail.com>
2021-04-03 12:38:01 +0300

    add contact form endpoint

--- src/server.js
+++ src/server.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, '../build')));
+
+app.post('/api/contactForm', async (req, res) => {
+  const { email, name, message } = req.body;
+  if (!email || !name || !message) {
+    throw new Error('Request not valid');
+  }
+
+  const msg = `
+    name:${name}<br />email:${email}<br />msg:${message}
+  `;
+
+  const result = await axios.post('https://www.sms4free.co.il/ApiSMS/SendSMS', {
+    key: 'C1dPmveuS',
+    user: '0548114549',
+    pass: '66887628',
+    sender: 'Portfolio',
+    recipient: '0548114540',
+    msg
+  });
+
+  if (result.data <= 0) {
+    console.log(`Error send sms, code: ${result.data}. msg: ${msg}`);
+    return res.send({ success: false });
+  }
+
+  res.send({ success: true });
+});
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
